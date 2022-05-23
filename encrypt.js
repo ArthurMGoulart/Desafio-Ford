7b3529b0458cdf4be72f9b299b7fc5a704c784e7
ArthurMGoulart <budiarthur@gmail.com>
2022-05-23 11:02:37 -0300

    docs: Encrypt documentation

--- encrypt.js
+++ encrypt.js
@@ -3,6 +3,7 @@ let result = "";
 
 let CESAR_KEY = 0;
 
+// Soma todos Números do VIM_NUMBER para obter a chave césar
 for (let i = 0; i < VIM_NUMBER.length; i++) {
   const currChar = VIM_NUMBER[i];
   if (!isNaN(currChar)) {
@@ -10,13 +11,16 @@ for (let i = 0; i < VIM_NUMBER.length; i++) {
   }
 }
 
+// Realiza um módulo de 10 para obter um número de 0 a 9 independente da soma
 CESAR_KEY = CESAR_KEY % 10;
 
 for (let i = 0; i < VIM_NUMBER.length; i++) {
+  // Transforma o character em ASCII soma 2 fixo + a chave de césar
   result += String.fromCharCode(VIM_NUMBER.charCodeAt(i) + 2 + CESAR_KEY);
 }
 
+// Adiciona a chave de césar para o lado do servidor saber descriptografar
 result += CESAR_KEY;
 
-console.log(result)
+console.log(result);
 
