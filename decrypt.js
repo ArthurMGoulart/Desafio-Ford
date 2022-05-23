e88b9d342c244f06c3ddca842fbfa993a1f4835e
ArthurMGoulart <budiarthur@gmail.com>
2022-05-23 11:21:58 -0300

    docs: english version

--- decrypt.js
+++ decrypt.js
@@ -1,10 +1,13 @@
 const decrypt = (encrypted) => {
+  // Get cesar key used to encrypt, that is on last index of encrypted message
   // Pega a chave de césar que está contida na última posição da mensagem encriptada
   const CESAR_KEY = parseInt(encrypted[encrypted.length - 1], 10);
+  // Remove last index
   // Retira a última posição
   encrypted = encrypted.slice(0, -1);
   let result = "";
   for (let i = 0; i < encrypted.length; i++) {
+    // Character to Ascii - 2 - cesar key and add to result string
     // Transforma o character em ASCII subtrai 2 fixo - a chave de césar
     result += String.fromCharCode(encrypted.charCodeAt(i) - 2 - CESAR_KEY);
   }
--- encrypt.js
+++ encrypt.js
@@ -5,10 +5,12 @@ const getCesarKey = (vimNumber) => {
   for (let i = 0; i < vimNumber.length; i++) {
     const currChar = vimNumber[i];
     if (!isNaN(currChar)) {
+      // Sum all numbers from VIM_NUMBER to get cesar key
       // Soma todos Números do VIM_NUMBER para obter a chave césar
       CESAR_KEY += parseInt(currChar, 10);
     }
   }
+  // Returns sum mod 10 to be a number between 0 and 9
   // Realiza um módulo de 10 para obter um número de 0 a 9 independente da soma
   return CESAR_KEY % 10;
 }
@@ -17,9 +19,13 @@ const encrypt = (vimNumber) => {
   let result = "";
   const CESAR_KEY = getCesarKey(vimNumber);
   for (let i = 0; i < VIM_NUMBER.length; i++) {
+    // Character to Ascii + 2 + cesar key and add to result string
     // Transforma o character em ASCII soma 2 fixo + a chave de césar
     result += String.fromCharCode(VIM_NUMBER.charCodeAt(i) + 2 + CESAR_KEY);
   }
+  // ! Business rule
+  // Add cesar key to the end of result for server to be able to decrypt.
+  // ! Regra de negócio
   // Adiciona a chave de césar para o lado do servidor saber descriptografar
   result += CESAR_KEY;
   return result;
