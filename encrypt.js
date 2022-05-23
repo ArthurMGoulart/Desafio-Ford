60831ff79db2162efbe28326620c68f378d39a17
ArthurMGoulart <budiarthur@gmail.com>
2022-05-23 11:06:13 -0300

    feat: getCesarKey and encrypt function

--- encrypt.js
+++ encrypt.js
@@ -1,26 +1,28 @@
 const VIM_NUMBER = "1HGCM82633A004352";
-let result = "";
 
-let CESAR_KEY = 0;
-
-// Soma todos Números do VIM_NUMBER para obter a chave césar
-for (let i = 0; i < VIM_NUMBER.length; i++) {
-  const currChar = VIM_NUMBER[i];
-  if (!isNaN(currChar)) {
-    CESAR_KEY += parseInt(currChar, 10);
+const getCesarKey = (vimNumber) => {
+  let CESAR_KEY = 0;
+  for (let i = 0; i < VIM_NUMBER.length; i++) {
+    const currChar = VIM_NUMBER[i];
+    if (!isNaN(currChar)) {
+      // Soma todos Números do VIM_NUMBER para obter a chave césar
+      CESAR_KEY += parseInt(currChar, 10);
+    }
   }
+  // Realiza um módulo de 10 para obter um número de 0 a 9 independente da soma
+  return CESAR_KEY % 10;
 }
 
-// Realiza um módulo de 10 para obter um número de 0 a 9 independente da soma
-CESAR_KEY = CESAR_KEY % 10;
-
-for (let i = 0; i < VIM_NUMBER.length; i++) {
-  // Transforma o character em ASCII soma 2 fixo + a chave de césar
-  result += String.fromCharCode(VIM_NUMBER.charCodeAt(i) + 2 + CESAR_KEY);
+const encrypt = (vimNumber) => {
+  let result = "";
+  const CESAR_KEY = getCesarKey(vimNumber);
+  for (let i = 0; i < VIM_NUMBER.length; i++) {
+    // Transforma o character em ASCII soma 2 fixo + a chave de césar
+    result += String.fromCharCode(VIM_NUMBER.charCodeAt(i) + 2 + CESAR_KEY);
+  }
+  // Adiciona a chave de césar para o lado do servidor saber descriptografar
+  result += CESAR_KEY;
+  return result;
 }
 
-// Adiciona a chave de césar para o lado do servidor saber descriptografar
-result += CESAR_KEY;
-
-console.log(result);
-
+console.log(encrypt(VIM_NUMBER));
